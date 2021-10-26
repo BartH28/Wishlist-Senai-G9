ca62ab2f6be210c086aceac433f43730fc57db2e
Rafaela Lucia <rafaluciadev@gmail.com>
2021-10-26 16:25:38 -0300

    requisicoes do postman

--- Api/wishlist-webAPI/Contexts/WishListContext.cs
+++ Api/wishlist-webAPI/Contexts/WishListContext.cs
@@ -14,7 +14,7 @@ namespace wishlist_webAPI.Contexts
         }
 
         public WishListContext(DbContextOptions<WishListContext> options)
-            : base(options)
+            : base(options) 
         {
         }
 
@@ -27,6 +27,7 @@ namespace wishlist_webAPI.Contexts
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=DESKTOP-PVCFVR0\\SQLEXPRESS; initial catalog=WishList; user Id=sa; pwd=senai@132;");
+                //optionsBuilder.UseSqlServer("Data Source=LAPTOP-RSG62TB1\\SQLEXPRESS; initial catalog=WishList; user Id=sa; pwd=Senai@132;");
             }
         }
 
