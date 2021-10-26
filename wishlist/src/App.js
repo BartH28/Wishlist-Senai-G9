e32a4b7c13e638e4b6cb07c5abf0bd50510f9556
Rafaela Lucia <rafaluciadev@gmail.com>
2021-10-26 17:34:57 -0300

    comeco react

--- wishlist/src/App.js
+++ wishlist/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+       listaDesejos: [],
+       descricao: '',
+      }
+  };
+
+  buscarDesejos = () => {
+    console.log('Fazer a chamada para a API')
+    fetch('http://localhost:5000/api/desejo')
+    .then(r => r.json()) 
+    .then(data => this.setState({listaDesejos: data}) )  
+    .catch(erro => console.log(erro)) 
+}
+
+cadastrarDesejos = (desejo) => {
+  desejo.preventDefault();
+
+  fetch('http://localhost:5000/api/desejo', {
+      method: 'POST', 
+      body: JSON.stringify({ descricao : this.state.descricao}),
+      headers :{
+          "Content-Type" : "application/json"
+      }
+  })
+  
+  .then(console.log("desejo cadastrado."))
+  .catch(erro => console.log(erro))  
+  .then(this.buscarDesejos)
+  .then(this.setState({ descricao: ''}))
+}
+
+componentDidMount(){
+  this.buscarDesejos()
+}
+
+
+  render() {
+    return(<div>
+      <h1>WishList</h1>
+      </div>
+      )
+  }
+
 }
 
 export default App;
